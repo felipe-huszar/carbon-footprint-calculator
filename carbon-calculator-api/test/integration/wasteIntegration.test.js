92e58086637788c59f2796b682cd2056b2c0d45c
felipe-huszar <felipe.huszar@hotmail.com>
2024-04-22 08:49:02 -0300

    waste integration tests

--- carbon-calculator-api/test/integration/wasteIntegration.test.js
+++ carbon-calculator-api/test/integration/wasteIntegration.test.js
@@ -7,97 +7,167 @@ const rootResolvers = require('../../src/graphql/rootResolvers');
 
 let expressApp;
 
-const query = `
-    mutation CalculateWasteEmission($input: CalculateWasteInput!) {
-        calculateWasteEmission(input: $input) {
-            wasteEmission
-            wasteReduction
+const configureInitialParametersMutation = `
+    mutation ConfigureInitialParameters {
+        configureInitialParameters(input: {
+            numberOfPeoplehousehold: 1,
+            zipCode: "11111"    
+        }) {
+            carbonFootprintSummary {
+                currentTotalEmission
+                currentTotalEmissionAfterPlannedActions
+                usAverage
+            }
         }
     }
 `;
 
-const variables = {
+const wasteMutation = `
+    mutation CalculateWasteEmission($input: CalculateWasteInput!) {
+        calculateWasteEmission(input: $input) {
+            waste {
+                wasteRecycling,
+                emissionAfterRecycling,
+                wasteReduction
+            }
+        }
+    }
+`;
+
+const wasteVariables = {
     input: {
         recycleAluminumSteelCans: true,
         recyclePlastic: true,
         recycleGlass: true,
         recycleNewspaper: true,
-        recycleMagazines: true,
+        recycleMagazines: false,
         reduction: {
-            recycleAluminumSteelCans: true,
-            recyclePlastic: true,
-            recycleGlass: true,
-            recycleNewspaper: true,
+            recycleAluminumSteelCans: false,
+            recyclePlastic: false,
+            recycleGlass: false,
+            recycleNewspaper: false,
             recycleMagazines: true
         }
     }
 };
 
-const expectedOutput = {
+const expectedWasteOutput = {
     "data": {
         "calculateWasteEmission": {
-            "wasteEmission": 925,
-            "wasteReduction": 458            
+            "waste": {
+                "wasteRecycling": 403,
+                "emissionAfterRecycling": 577,
+                "wasteReduction": 55
+            },
         }
-    }
-}
+    }    
+};
 
 describe('Waste Emission Integration', () => {
-    beforeAll(() => {
-        expressApp = express();
-        expressApp.use(
-            '/graphql',
-            graphqlHTTP({
-                schema: schema,
-                rootValue: rootResolvers,
-                graphiql: false,
-            })
-        );    
-        jest.setTimeout(10000);
+    describe('without initial parameters configured', () => {
+        beforeAll(() => {
+            expressApp = express();
+            expressApp.use(
+                '/graphql',
+                graphqlHTTP({
+                    schema: schema,
+                    rootValue: rootResolvers,
+                    graphiql: false,
+                })
+            );    
+            jest.setTimeout(10000);
+        });
+        
+        it('returns 200 with correct error message when parameters have not been configured correctly', (done) => {
+            request(expressApp)
+                .post('/graphql')
+                .send({
+                    operationName: 'CalculateWasteEmission',
+                    query: wasteMutation,
+                    variables: wasteVariables
+                })
+                .expect(200)
+                .end((err, res) => {
+                    if (err) return done(err);
+                        
+                    expect(res.body.errors).toBeDefined();
+                    expect(res.body.errors[0].message).toEqual('Initial parameters have not been configured');
+                    
+                    done();
+                });
+        });        
     });
+
+    describe('with initial parameters configured', () => {
+        beforeAll(() => {
+            expressApp = express();
+            expressApp.use(
+                '/graphql',
+                graphqlHTTP({
+                    schema: schema,
+                    rootValue: rootResolvers,
+                    graphiql: false,
+                })
+            );    
+            jest.setTimeout(10000);
+        });
+        
+        beforeEach((done) => {
+            request(expressApp)
+                .post('/graphql')
+                .send({
+                    operationName: 'ConfigureInitialParameters',
+                    query: configureInitialParametersMutation,
+                })
+                .end((err, res) => {
+                    if (err) return done(err);
+                    done();
+                });
+        });
+
+        it('returns 200 when correctly passing parameters and calculates emissions correctly', (done) => {
+            request(expressApp)
+                .post('/graphql')
+                .send({
+                    operationName: 'CalculateWasteEmission',
+                    query: wasteMutation,
+                    variables: wasteVariables
+                })
+                .expect(200)
+                .end((err, res) => {
+                    if (err) return done(err);
+                                  
+                    expect(res.body).toEqual(expectedWasteOutput);
+                    done();
+                });
+        });
     
-    it('returns 200 when correctly passing parameters and calculates emissions correctly', (done) => {
-        request(expressApp)
-            .post('/graphql')
-            .send({
-                operationName: 'CalculateWasteEmission',
-                query,
-                variables
-            })
-            .expect(200)
-            .end((err, res) => {
-                if (err) return done(err);
-                              
-                expect(res.body).toEqual(expectedOutput);
-                done();
-            });
-    });
-
-    it('returns 400 when the query is malformed', (done) => {
-        request(expressApp)
-            .post('/graphql')
-            .send({
-                operationName: 'CalculateWasteEmission',
-                query: 'random query',
-                variables
-            })
-            .expect(400)
-            .end((err, res) => {
-                return done(err);
-            });
-    });
-
-    it('returns 500 when the input is malformed', (done) => {
-        request(expressApp)
-            .post('/graphql')
-            .send({
-                operationName: 'CalculateWasteEmission',
-                query,
-                variables: { randomData: 'abc' }
-            })
-            .expect(500)
-            .end((err, res) => {
-                return done(err);
-            });
-    });
+        it('returns 400 when the query is malformed', (done) => {
+            request(expressApp)
+                .post('/graphql')
+                .send({
+                    operationName: 'CalculateWasteEmission',
+                    query: 'random query',
+                    variables: wasteVariables
+                })
+                .expect(400)
+                .end((err, res) => {
+                    return done(err);
+                });
+        });
+    
+        it('returns 500 when the input is malformed', (done) => {
+            request(expressApp)
+                .post('/graphql')
+                .send({
+                    operationName: 'CalculateWasteEmission',
+                    query: wasteMutation,
+                    variables: { randomData: 'abc' }
+                })
+                .expect(500)
+                .end((err, res) => {
+                    return done(err);
+                });
+        });        
+    });      
 });
